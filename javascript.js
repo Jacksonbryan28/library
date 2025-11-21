8d790b12658ea5ed3c39bd86d4d9075ab082b0c8
Jackson <jacksonbryan280@gmail.com>
2025-11-21 00:01:19 -0500

    Added addBookToLibrary function appening each library book to the DOM

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const cardContainer = document.querySelector(".container");
 
 function Book(title, author, pages, hasRead) {
   if (!new.target) {
@@ -23,10 +24,6 @@ function Book(title, author, pages, hasRead) {
 }
 
 function addBookToLibrary(title, author, pages, hasRead) {
-  //   if (!new.target) {
-  //     throw Error("You must use the 'new' operator to call the constructor");
-  //   }
-
   const newBook = new Book(title, author, pages, hasRead);
   myLibrary.push(newBook);
 }
@@ -40,18 +37,43 @@ const theHobbit = new Book(
   "not read yet"
 );
 
-// const gameOfThrones = new addBookToLibrary(
-//   "Game of thrones",
-//   "George R.R. Martin",
-//   "784",
-//   "not read yet"
-// );
 addBookToLibrary(
   "Game of thrones",
   "George R.R. Martin",
   "784",
   "not read yet"
 );
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkein", "295", "not read yet");
+
+function bookDisplay() {
+  myLibrary.forEach((book) => {
+    //Adds book elements to dom and adds appropriate styling
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.dataset.id = book.ID;
+
+    const title = document.createElement("h3");
+    title.classList.add("title");
+    title.textContent = book.title;
+    const author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent = book.author;
+    const pageNum = document.createElement("p");
+    pageNum.classList.add("pages");
+    pageNum.textContent = book.pages;
+    const read = document.createElement("p");
+    read.classList.add("read");
+    read.textContent = book.hasRead;
+
+    //appends it to the correct location
+    cardContainer.appendChild(newCard);
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pageNum);
+    newCard.appendChild(read);
+  });
+}
 
 console.log(theHobbit.info());
 console.log(theHobbit.ID);
@@ -60,5 +82,4 @@ console.log("------------------------");
 console.log(myLibrary[0]);
 console.log(myLibrary[0].info());
 
-
-
+bookDisplay();
